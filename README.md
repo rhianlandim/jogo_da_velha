6f5381a584ca2fca2d851254fc64470ac2c54c96
Rhian Moura <rhian.moura@pagonxt.com>
2023-06-01 23:07:47 -0300

    finalizado

---
+++ README.md
@@ -0,0 +1,16 @@
+# JOGO DA VELHA VIRTUAL
+
+## DESCRIÇÃO
+
+O comumente conhecido "Jogo da Velha" ganhou popularidade no século 19, na Inglaterra.
+Que por coincidência era praticado por senhoras, ou "velhas", senhoras das quais não tinham mais capacidade de bordar e tricotar, atividade praticada pelas mulheres da época em seus momentos de lazer em conjunto. Assim, o jogo rodou o mundo e se tornou uma brincadeira de criança super divertida e simples de praticar.
+
+## TECNOLOGIAS UTILIZADAS
+
+![image](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![image](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![image](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+## EXECUÇÃO
+
+A execução é bem simples. Clone o repositório para sua máquina ou faça o download zip. Em seguida, entre na pasta e abra qualquer um dos arquivos ".html" em seu navegador. De preferência, abra o "index.html". Depois de realizar esses passos, é só se divertir!
---
+++ jogo/jogo.js
@@ -0,0 +1,72 @@
+const jogadorAtual = document.querySelector(".jogadorAtual");
+const resultado = document.querySelector(".resultado");
+
+let escolhido;
+let jogador = "X";
+
+let trincas = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function inicia() {
+  escolhido = [];
+
+  jogadorAtual.innerHTML = `"${jogador}" COMEÇA! `;
+
+  document.querySelectorAll(".jogo button").forEach((quadrado) => {
+    quadrado.innerHTML = "";
+    quadrado.addEventListener("click", marcar);
+  });
+}
+
+inicia();
+
+function marcar(quadrado) {
+  const index = quadrado.target.getAttribute("data-index");
+  quadrado.target.innerHTML = jogador;
+  resultado.innerHTML= "RESULTADO: ";
+  quadrado.target.removeEventListener("click", marcar);
+  escolhido[index] = jogador;
+
+  setTimeout(() => {
+    check();
+  }, [100]);
+
+  jogador = jogador === "X" ? "O" : "X";
+  jogadorAtual.innerHTML = `JOGADOR DA VEZ: ${jogador}`;
+}
+
+function check() {
+  let ultimaJogada = jogador === "X" ? "O" : "X";
+
+  const quadrados = escolhido
+    .map((quadrado, index) => [quadrado, index])
+    .filter((quadrado) => quadrado[0] === ultimaJogada)
+    .map((quadrado) => quadrado[1]);
+
+  for (let trinca of trincas) {
+    if (trinca.every((quadrado) => quadrados.includes(quadrado))) {
+      resultado.innerHTML = `RESULTADO: "${ultimaJogada}" VENCEU!`;
+
+      setTimeout(() => {
+        inicia();
+      }, [2000]);
+      return;
+    }
+  }
+
+  if (escolhido.filter((quadrado) => quadrado).length === 9) {
+    resultado.innerHTML= "RESULTADO: EMPATE!";
+    
+    setTimeout(() => {
+      inicia();
+    }, [2000]);
+  }
+}
